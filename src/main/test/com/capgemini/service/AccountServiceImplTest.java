e9d431d908a57cbc23790aad48963ada780bd5e4
sapraksh <sapraksh@DIN51001998.corp.capgemini.com>
2017-04-19 12:03:15 +0530

    Added Test Class

--- src/main/test/com/capgemini/service/AccountServiceImplTest.java
+++ src/main/test/com/capgemini/service/AccountServiceImplTest.java
@@ -1,5 +1,32 @@
 package com.capgemini.service;
 
-public class AccountServiceImplTest {
+import org.junit.Test;
 
+import com.capgemini.beans.Account;
+import com.capgemini.exceptions.InsufficientInitialAmountException;
+import com.capgemini.exceptions.InvalidAccountNumberException;
+
+public class AccountServiceImplTest {
+	@Test(expected = InsufficientInitialAmountException.class)
+	public void testCreateAccountShouldFailWhenAmmountLessThan5000() throws InsufficientInitialAmountException, InvalidAccountNumberException{
+		Account acc = new Account();
+		acc.setAccountNumber(1);
+		acc.setAmount(1000);
+		AccountService accServ = new AccountServiceImpl();
+		accServ.createAccount(acc);
+	}
+	
+	@Test(expected = InvalidAccountNumberException.class)
+	public void testCreateAccountShouldFailWhenAccountNumberIsDuplicate() throws InsufficientInitialAmountException, InvalidAccountNumberException{
+		AccountService accServ = new AccountServiceImpl();
+		Account acc1 = new Account();
+		acc1.setAccountNumber(1);
+		acc1.setAmount(5000);
+		accServ.createAccount(acc1);
+		
+		Account acc2 = new Account();
+		acc2.setAccountNumber(1);
+		acc2.setAmount(5000);
+		accServ.createAccount(acc1);
+	}
 }
